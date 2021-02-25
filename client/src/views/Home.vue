f674078266c281c7bfbd46e654bb4623bc006c42
Jared Finn <jaredhfinn@gmail.com>
2021-02-25 14:11:32 -0500

    Add font awesome and beaufy

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" id="home-image">
+    <h1>Hi this is new code</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
