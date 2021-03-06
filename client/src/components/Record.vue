75848996a55ec1087c30000022256594b313cb97
Jared Finn <jaredhfinn@gmail.com>
2021-03-06 17:55:11 -0500

    Began posting process form record to feed

--- client/src/components/Record.vue
+++ client/src/components/Record.vue
@@ -1,36 +1,63 @@
 <template>
-    <div class="feed">
-        <article class="box">
-            <div class="progress-box">
-                <p class="progress-percent">60%</p>
-                <progress class="progress is-success" value="60" max="100">60%</progress>
-            </div>
-                
-            <article class="panel is-primary">
-            <p class="panel-heading">
-                Primary
-            </p>
-            <p class="panel-tabs">
-                <a class="is-active">Run/Walk</a>
-                <a>Swim</a>
-                <a>Bike</a>
-                <a>Gym</a>
-                <a>Ski/Snowboard</a>
-            </p>
-
-            <div class="field">
-                <label class="label-title">Title</label>
-                <div class="control">
-                <input class="input" type="text" placeholder="Give your workout a name!">
+    <div class="record">
+        <form @submit.prevent="addPost">
+            <article class="box">
+                <div class="progress-box">
+                    <p class="progress-percent">You are <strong class="is-size-4">60%</strong> done with your daily goal!</p>
+                    <progress class="progress is-success" value="60" max="100">60%</progress>
                 </div>
-            </div>
-            
-            <div class="columns">
-                <div class="column is half">
+                    
+                <article class="panel is-info">
+                <p class="panel-heading">
+                    Record Your Workout
+                </p>
+                <p class="panel-tabs">
+                    <a class="exercises is-active">Run/Walk</a>
+                    <a class="exercises">Swim</a>
+                    <a class="exercises">Bike</a>
+                    <a class="exercises">Gym</a>
+                    <a class="exercises">Ski/Snowboard</a>
+                </p>
+
                 <div class="field">
-                    <label class="label">Distance</label>
+                    <label class="label">Title</label>
+                    <div class="control">
+                    <input class="input" type="text" placeholder="Give your workout a name!" >
+                    </div>
+                </div>
+                
+                <div class="columns">
+                    <div class="column is half">
+                    <div class="field">
+                        <label class="label">Distance</label>
+                        <div class="control has-icons-left has-icons-right">
+                        <input class="input" type="text" placeholder="E.g. 4 miles">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-user"></i>
+                        </span>
+                        <span class="icon is-small is-right">
+                            <i class="fas fa-check"></i>
+                        </span>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="column is half">
+                    <div class="field">
+                        <label class="label">Time</label>
+                        <div class="control has-icons-left has-icons-right">
+                        <input class="input" type="text" placeholder="How long did it take?">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-stopwatch"></i>
+                        </span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Anyone Join?</label>
                     <div class="control has-icons-left has-icons-right">
-                    <input class="input" type="text" placeholder="E.g. 4 miles">
+                    <input class="input" type="text" placeholder="Tag those that joined you!">
                     <span class="icon is-small is-left">
                         <i class="fas fa-user"></i>
                     </span>
@@ -39,67 +66,42 @@
                     </span>
                     </div>
                 </div>
-                </div>
-                <div class="column is half">
+                
                 <div class="field">
-                    <label class="label">Time</label>
-                    <div class="control has-icons-left has-icons-right">
-                    <input class="input" type="text" placeholder="How long did it take?">
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-stopwatch"></i>
-                    </span>
+                    <label class="label">Difficulty</label>
+                    <div class="control">
+                        <div class="select is-info">
+                            <select>
+                                <option>Select Option</option>
+                                <option>Too Easy</option>
+                                <option>Easy</option>
+                                <option>Just Right</option>
+                                <option>Hard</option>
+                                <option>Too Hard</option>
+                                <option>Recovery</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
+                
+                <div class="field">
+                    <label class="label">Caption</label>
+                    <div class="control">
+                    <textarea class="textarea" placeholder="Share your experience!"></textarea>
+                    </div>
                 </div>
-            </div>
-
-            <div class="field">
-                <label class="label">Anyone Join?</label>
-                <div class="control has-icons-left has-icons-right">
-                <input class="input" type="text" placeholder="Tag those that joined you!">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-user"></i>
-                </span>
-                <span class="icon is-small is-right">
-                    <i class="fas fa-check"></i>
-                </span>
+                
+                <div class="field is-grouped">
+                    <div class="control">
+                    <button class="button is-info">Post</button>
+                    </div>
+                    <div class="control">
+                    <button class="button is-info is-light">Cancel</button>
+                    </div>
                 </div>
-            </div>
-            
-            <div class="field">
-                <label class="label">Difficulty</label>
-                <div class="control">
-                <div class="select">
-                    <select>
-                    <option>Select Option</option>
-                    <option>Too Easy</option>
-                    <option>Easy</option>
-                    <option>Just Right</option>
-                    <option>Hard</option>
-                    <option>Too Hard</option>
-                    <option>Recovery</option>
-                    </select>
-                </div>
-                </div>
-            </div>
-            
-            <div class="field">
-                <label class="label">Comment</label>
-                <div class="control">
-                <textarea class="textarea" placeholder="Share your experience!"></textarea>
-                </div>
-            </div>
-            
-            <div class="field is-grouped">
-                <div class="control">
-                <button class="button is-link">Submit</button>
-                </div>
-                <div class="control">
-                <button class="button is-link is-light">Cancel</button>
-                </div>
-            </div>
+                </article>
             </article>
-        </article>
+        </form>
     </div>
     
 </template>
@@ -126,5 +128,8 @@ export default {
 }
 .control{
     margin-bottom: 10px;
+}
+.exercises{
+    color: gray;
 }
 </style>
---
+++ client/src/components/newPost.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+    <article v-for="post in posts" :key="post"  class="media">
+      <figure class="media-left">
+          <p class="image is-64x64">
+            <img  id="profile-image" src="https://bulma.io/images/placeholders/128x128.png">
+          </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>{{ post.name + " " }}</strong>
+            <small>{{ post.account }}</small>
+            <br>
+              {{ post.status }}
+            <br>
+          </p>
+            <div class="box" width="50">
+              <div class="card-image">
+                <figure class="image is-16by9">
+                  <img class="has-ratio" frameborder="0" allowfullscreen src="https://wvtourism.com/wp-content/uploads/2018/04/Endles-Wall-4-13-18.jpg" alt="Placeholder image">              
+                </figure>
+              </div>
+            </div>
+          <small><a>Like</a> · <a>Reply</a> · {{ post.lastTime }}</small>
+        </div>
+
+        <article class="media">
+          <figure class="media-left">
+            <p class="image is-48x48">
+              <img src="https://bulma.io/images/placeholders/96x96.png">
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>Sean Brown</strong>
+                <br>
+                Donec sollicitudin urna eget eros malesuada sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam blandit nisl a nulla sagittis, a lobortis leo feugiat.
+                <br>
+                <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
+              </p>
+            </div>
+          </div>
+        </article>
+
+        <article class="media">
+          <figure class="media-left">
+            <p class="image is-48x48">
+              <img src="https://bulma.io/images/placeholders/96x96.png">
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>Kayli Eunice </strong>
+                <br>
+                Sed convallis scelerisque mauris, non pulvinar nunc mattis vel. Maecenas varius felis sit amet magna vestibulum euismod malesuada cursus libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus lacinia non nisl id feugiat.
+                <br>
+                <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
+              </p>
+            </div>
+          </div>
+        </article>
+      </div>
+    </article>
+
+    <article class="media">
+      <figure class="media-left">
+        <p class="image is-64x64">
+          <img src="https://bulma.io/images/placeholders/128x128.png">
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="field">
+          <p class="control">
+            <textarea class="textarea" placeholder="Add a comment..."></textarea>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control">
+            <button class="button">Post comment</button>
+          </p>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        newpost: {
+            user: { }
+        },
+        posts: [
+            {
+                name:"",
+                distance:"",
+                time:"",
+                fellowFitter:"",
+                difficulty:"",
+                status:"",
+            }
+        ]   
+    }),
+
+    methods: {
+        addPost(){
+            this.posts.unshift(this.newPost);
+            this.newPost = { user: {} }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
