9afbb5825942f365ba046845bef7aca7ccf92f07
Jared Finn <jaredhfinn@gmail.com>
2021-03-25 00:27:18 -0400

    explore page attempt

--- client/src/views/Explore.vue
+++ client/src/views/Explore.vue
@@ -2,16 +2,25 @@
 <div>
     <section class="hero is-info is-fullwidth">
     <div class="hero-body">
-        <p class="title has-text-centered">
-                Explore
-        </p>
-    </div>
-
-        <div class="columns">
-            <div class="column is-one-third" v-for="(post, i) in posts" :key="i"  >
-                <!--displays only 3 most recent posts-->
-                <content-card :post="post"/>
+          <div class="container">
+            <p class="title has-text-centered">
+            Fit some Fitness in today.
+            </p>
+            <p class="subtitle has-text-centered">
+            Record and share your exercises!
+            </p>
+            <div class="columns" >
+                <div class="column is-one-third" v-for="(post, i) in posts" :key="i" >
+                    <div v-if="i % 3 === 0">
+                        <content-card  :post="post"/>
+                    </div>
+                    
+                    <div v-else>
+                        <content-card :post="post"/>
+                    </div>
+                </div>
             </div>
+          </div>
         </div>
     </section>
 </div>
@@ -33,4 +42,5 @@ export default {
 </script>
 
 <style>
+
 </style>
