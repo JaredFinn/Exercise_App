6422c6232cbdfbd8576560750747ab688c097218
Jared Finn <jaredhfinn@gmail.com>
2021-04-12 22:59:30 -0400

    client connection with server

--- client/src/models/Posts.js
+++ client/src/models/Posts.js
@@ -1,70 +1,19 @@
 /*
-
+    Access to all the posts
 */
 
-import Session from "./Session";
+//import Session from "./Session";
+import { api } from "../models/myFetch";
 
-const posts = [
-    {
-        user: "IsabellaP",
-        userHandle: "@Isabel5",
-        sport: "Run-Walk",
-        title:"Morning walk to get some sunshine!",
-        distance:"1 mile",
-        time: "30 min",
-        fellowFitter:"",
-        difficulty:"Just Right",
-        status:"Nice morning to get up and walk!",
-        img:"https://cdn.cdnparenting.com/articles/2018/11/04122759/562075621-H.jpg"
-    },
-    {
-        user: "LeoLit",
-        userHandle: "@LeoLit",
-        sport: "Run/Walk",
-        title:"Afternoon Hike To Awosting Falls!",
-        distance:"3 Miles",
-        time: "2 hours",
-        fellowFitter:"@Leo, @Luke",
-        difficulty:"Recovery",
-        status:"Beautiful afternoon hike in New Paltz. Took a loop to awosting falls!",
-        img:"https://i.ytimg.com/vi/xDn_vUuJVyo/maxresdefault.jpg"
-    },
-    {
-        user: "LukeGamboli",
-        userHandle: "@LG2200",
-        sport: "Swim",
-        title:"Morning Practice, Yikes..",
-        distance:"8300 yards",
-        time: "2 hours",
-        fellowFitter:"@HawksSwim",
-        difficulty:"Too Hard",
-        status:"Was a rough morning practice but feeling good!",
-        img:"https://i1.wp.com/www.swimmersdaily.com/wp-content/uploads/2019/09/a-swim-practice-surprise-cody-mi.jpg?fit=1280%2C720&ssl=1"
-    },
-    {
-        user: "JaredFinn",
-        userHandle: "@JaredFinn",
-        sport: "Ski-Snowboard",
-        title:"Big Shred Day Today!",
-        distance:"12 runs",
-        time: "6 hours",
-        fellowFitter:"@Leo, @Akiva",
-        difficulty:"Just Right",
-        status:"Hit up hunter mountain today, no friends on a pow day!",
-        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Hunter_Mountain.jpg/240px-Hunter_Mountain.jpg"
-    },
-    
-];
 
 export function GetMyPosts(){
-    return GetPostsForUser(Session.currentUserHandle);
+    return api("posts")
 }
 
-export function GetPostsForUser(id){
-    return posts.filter( x => x.userHandle == id);
+export function GetPostsForUser(handle){
+    return api(`posts/${handle}`)
 }
 
 export function GetMyFeed(){
-    return posts;
+    return api("posts/feed")
 }
-    
--- client/src/models/Session.js
+++ client/src/models/Session.js
@@ -1,20 +1,36 @@
 /* JF
     Holds the user session. Is a singleton.
 */
+import { ToastProgrammatic as toaster } from "buefy";
+import { api } from "./myFetch";
+
 const Session = {
-    currentUser: null,
-    currentUserHandle: null,
-    
+    user: null,
     message: [] // To be used later
     
 }
 export default Session;
 
-export function Login(user, handle){
-    Session.currentUser = user;
-    Session.currentUserHandle = handle;
+export async function Login(handle, password) {
+    const {user, token} = await api('users/login', { handle, password } )
+    Session.user = user;
+    Session.token = token;
+
+    console.log(Session.user);
+    toaster.open({
+        type: "is-success",
+        message: `Welcome ${Session.user.firstName}`
+    })
 }
 
 export function Logout() {
-    Session.currentUser = null
+    Session.user = null
+}
+
+export function toastError(msg){
+    toaster.open({
+        message: msg,
+        queue: false,
+        type: "is-danger"
+    })
 }
