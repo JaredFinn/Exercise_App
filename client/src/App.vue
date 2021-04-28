85e9c93504e4ad2534e4d422e738d78bb0130902
Jared Finn <jaredhfinn@gmail.com>
2021-04-28 14:22:05 -0400

    add FB chat

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,6 +5,13 @@
           <router-view/>
         </div>
         <MyFooter/>
+
+        <div id="fb-root"></div>
+        <div class="fb-customerchat"
+          attribution="page_inbox"
+          page_id="106698941567337">
+        </div>
+
     </div>
 </template>
 
@@ -18,7 +25,26 @@ export default {
     MyFooter
   }  
 }
+
+/*global FB*/
+window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
 </script>
 <style lang="scss">
 
 </style>
+
+      
