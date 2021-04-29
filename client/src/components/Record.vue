43005a30962458d8159aba8ecfe14b79b689f38f
Jared Finn <jaredhfinn@gmail.com>
2021-04-28 22:33:39 -0400

    commented function to display error message when logged out of record

--- client/src/components/Record.vue
+++ client/src/components/Record.vue
@@ -14,11 +14,29 @@
                     <li class="panel-tabs" >
                         <a v-for="tab in tabs" :key="tab" @click="selectTab(tab.title)" class="exercises" :class="{ 'is-active': selectedTab === tab.title }">{{ tab.title }}</a>
                     </li>
-                    <component @update-goal="updateGoal" v-bind:is="selectedTab" :sport="selectedTab"></component>                   
+                    <div v-if="Session.user">
+                        <component @update-goal="updateGoal" v-bind:is="selectedTab" :sport="selectedTab"></component>       
+                    </div>
+                    <!--<div v-else>
+                        <article class="message" id="login-message">
+                            <div class="message-header">
+                                <p>Daily Activity cannot be reached</p>
+                            </div>
+                            <div class="message-body has-text-centered is-size-6">
+                                You must be logged in to see your daily Activity.
+                            </div>
+                            <div class="has-text-centered">
+                                <router-link to="/login" class="button is-info has-text-centered">
+                                        Log in Here!
+                                </router-link>
+                            </div>
+                        </article>
+                    </div>    -->          
                 </article>
             </article>
         </form>
     </div>
+    
 </template>
 
 <script>
@@ -27,6 +45,7 @@ import Bike from "../components/Exercises/Bike"
 import Swim from "../components/Exercises/Swim"
 import SkiSnowboard from "../components/Exercises/Ski-Snowboard"
 import Gym from "../components/Exercises/Gym"
+import Session from "../models/Session";
 
 
 export default {
@@ -44,6 +63,7 @@ export default {
             
         },
         goal: 0,
+        Session
     }),
     components: {
         RunWalk,
@@ -88,4 +108,5 @@ export default {
 .exercises{
     color: gray;
 }
+
 </style>
